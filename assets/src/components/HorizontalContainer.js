9ff491334fe744829aef0eb09a25d783020a355d
Arjan Scherpenisse <arjan@scherpenisse.net>
2017-05-19 13:48:56 +0200

    Save splitter size to localStorage

--- assets/src/components/HorizontalContainer.js
+++ assets/src/components/HorizontalContainer.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import SplitLayout from 'react-split-layout'
+import debounce from 'lodash/debounce'
 
 export default class extends React.Component {
+
+  componentWillMount() {
+    this._sizes = '_sizes' in localStorage ? JSON.parse(localStorage._sizes) : [150, 400, null]
+  }
+
+  onChange = debounce((arg) => {
+    localStorage._sizes = JSON.stringify(arg)
+  }, 200)
+
   render() {
     let children = React.Children.toArray(this.props.children)
     if (children.length < 3) children.push(<div key={1}></div>)
+
     return (
       <div className="horizontal-container--wrapper">
         <SplitLayout
-          initialSizes={[200, 400, null]}
+          onChange={::this.onChange}
+          initialSizes={this._sizes}
           direction="vertical" >
           {children}
         </SplitLayout>
